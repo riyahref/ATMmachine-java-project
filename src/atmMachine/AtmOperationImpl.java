62f6752b000926d37a1dccb8943ae5b573d6789d
Riya Panjwani <riyapanjwani8020@gmail.com>
2025-09-24 22:31:56 +0530

    first commit

---
+++ src/atmMachine/AtmOperationImpl.java
@@ -0,0 +1,45 @@
+package atmMachine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AtmOperationImpl implements AtmOperationInterf{
+    ATM atm=new ATM();
+    Map<Double,String> ministat=new HashMap<>();
+    @Override
+    public void viewBalance() {
+        System.out.println("Available Balance is : "+atm.getBalance());
+
+    }
+
+    @Override
+    public void withdrawAmount(double withdrawAmount) {
+        if(withdrawAmount<= atm.getBalance()) {
+            ministat.put(withdrawAmount, " Amount Withdrawn");
+            System.out.println("Collect the Cash " + withdrawAmount);
+            atm.setBalance(atm.getBalance() - withdrawAmount);
+            viewBalance();
+        }
+        else{
+            System.out.println("Insufficient Balance !!");
+        }
+
+    }
+
+    @Override
+    public void depositAmount(double depositAmount) {
+        ministat.put(depositAmount, "Amount Deposited");
+        System.out.println(depositAmount+" Deposited Successfully !!");
+        atm.setBalance(atm.getBalance()+depositAmount);
+        viewBalance();
+
+    }
+
+    @Override
+    public void viewMiniStatement() {
+        for(Map.Entry<Double,String> m:ministat.entrySet()){
+            System.out.println(m.getKey()+""+m.getValue());
+        }
+
+    }
+}
